7f4fce95f28bac0e348f91deab5ed1e4566c05e5
KimDoYoon <150874253+doyoon530@users.noreply.github.com>
2026-03-18 17:55:43 +0900

    Update start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -4,6 +4,7 @@ setlocal
 if "%HOST%"=="" set HOST=0.0.0.0
 if "%PORT%"=="" set PORT=5000
 if "%WAITRESS_THREADS%"=="" set WAITRESS_THREADS=8
+if "%LLM_PROVIDER%"=="" set LLM_PROVIDER=local
 
-echo Starting NCAI server on http://%HOST%:%PORT%
+echo Starting NCAI server on http://%HOST%:%PORT%  [provider=%LLM_PROVIDER%]
 python app.py
